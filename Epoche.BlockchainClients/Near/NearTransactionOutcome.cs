1871bd522fc5de8abf4e2f1514ffb87a62bdd567
Josh <josh@joshlang.ca>
2023-09-18 15:51:19 -0700

    more tx fields

---
+++ Epoche.BlockchainClients/Near/NearTransactionOutcome.cs
@@ -0,0 +1,28 @@
+﻿namespace Epoche.BlockchainClients.Near;
+public class NearTransactionOutcome
+{
+    public class OutcomeInner
+    {
+        public class OutcomeStatus
+        {
+            public string SuccessReceiptId { get; init; } = default!;
+        }
+        [JsonPropertyName("executor_id")]
+        public string Executor { get; init; } = default!;
+        [JsonPropertyName("gas_burnt")]
+        public long GasBurnt { get; init; }
+        [JsonPropertyName("tokens_burnt")]
+        public string TokensBurnt { get; init; } = default!;
+        [JsonPropertyName("status")]
+        public OutcomeStatus Status { get; init; } = default!;
+    }
+
+    [JsonPropertyName("block_hash")]
+    public string BlockHash { get; init; } = default!;
+
+    [JsonPropertyName("id")]
+    public string Id { get; init; } = default!;
+
+    [JsonPropertyName("outcome")]
+    public OutcomeInner Outcome { get; init; } = default!;
+}
--- Epoche.BlockchainClients/Near/NearTransactionResult.cs
+++ Epoche.BlockchainClients/Near/NearTransactionResult.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Epoche.BlockchainClients.Near;
+﻿namespace Epoche.BlockchainClients.Near;
 public class NearTransactionResult
 {
     [JsonPropertyName("receipts_outcome")]
     public NearReceiptOutcome[] ReceiptsOutcome { get; init; } = default!;
+    [JsonPropertyName("transaction_outcome")]
+    public NearTransactionOutcome TransactionOutcome { get; init; } = default!;
+
+    [JsonIgnore] public string Hash => TransactionOutcome.Id;
 }
